177a981f30d26dd36d99202da2be89f531863856
Jeon ChoongHeon <beaverknight@aceproject.co.kr>
2019-05-09 10:40:27 +0900

    예제

---
+++ ProceduralPuzzle/Assets/Scripts/moveObject.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MOVE_DIR{
+    right = 0,
+    left,
+    forward,
+    backward
+}
+
+public class moveObject : MonoBehaviour {
+
+    public MOVE_DIR moveDir;
+    Vector3 moveVec;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(updateCoroutine());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        switch (moveDir)
+        {
+            case MOVE_DIR.right:
+                transform.eulerAngles = new Vector3(0, 90, 0);
+                moveVec = Vector3.right;
+                break;
+
+            case MOVE_DIR.left:
+                transform.eulerAngles = new Vector3(0, 270, 0);
+                moveVec = Vector3.left;
+                break;
+
+            case MOVE_DIR.forward:
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                moveVec = Vector3.up;
+                break;
+
+            case MOVE_DIR.backward:
+                transform.eulerAngles = new Vector3(0, 360, 0);
+                moveVec = Vector3.down;
+            break;
+        }
+
+        Debug.DrawRay(transform.position + moveVec + Vector3.back, Vector3.forward * 10.0f, Color.red);
+	}
+
+    void work()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position + moveVec + Vector3.back,Vector3.forward * 10.0f, out hit))
+        {
+            Debug.Log(hit.transform.name);
+            transform.position = hit.transform.position;
+        }
+    }
+
+    IEnumerator updateCoroutine()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(2.0f);
+
+            work();
+        }
+    }
+}
