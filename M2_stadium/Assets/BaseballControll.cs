04618719f48e10cfd7e3c684dc59dc44c8e9668c
Jeon ChoongHeon <beaverknight@aceproject.co.kr>
2019-08-30 17:22:04 +0900

    키애니 추가

--- M2_stadium/Assets/BaseballControll.cs
+++ M2_stadium/Assets/BaseballControll.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseballControll : MonoBehaviour {
+public class BaseballControll : MonoBehaviour
+{
 
     public static BaseballControll instance = null;              //Static instance of GameManager which allows it to be accessed by any other script.
 
@@ -26,22 +27,22 @@ public class BaseballControll : MonoBehaviour {
         renderer = GetComponent<MeshRenderer>();
     }
 
-	void Start () 
+    void Start()
     {
         //MoveByPaths();
-	}
-	
-	// Update is called once per frame
-	void Update () 
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         if (Input.GetMouseButton(0) && Input.GetKey(KeyCode.E))
         {
             Vector3 pos = GetClickedObject(hitterPaths[hitterPaths.Length - 1].position);
             hitterPaths[hitterPaths.Length - 1].position = pos;
         }
-	}
+    }
 
-    private Vector3 GetClickedObject(Vector3 defaultPos) 
+    private Vector3 GetClickedObject(Vector3 defaultPos)
     {
         //충돌이 감지된 영역
         RaycastHit hit;
@@ -49,15 +50,15 @@ public class BaseballControll : MonoBehaviour {
         //마우스 포이트 근처 좌표를 만든다.
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         //마우스 근처에 오브젝트가 있는지 확인
-        if((Physics.Raycast(ray, out hit, 100.0f))) 
+        if ((Physics.Raycast(ray, out hit, 100.0f)))
         {
-            if(hit.collider.tag == "Field")
+            if (hit.collider.tag == "Field")
             {
                 return hit.point;
             }
         }
         return defaultPos;
-    } 
+    }
 
     public void MoveByPaths()
     {
@@ -66,7 +67,7 @@ public class BaseballControll : MonoBehaviour {
 
     public void MoveToTargetPos(Vector3 destPos)
     {
-        
+
         iTween.MoveTo(gameObject, iTween.Hash("position", destPos,
                                               "easeType", iTween.EaseType.easeOutQuad,
                                               "time", 0.5f,
@@ -83,20 +84,29 @@ public class BaseballControll : MonoBehaviour {
 
         Debug.Log(isHitted ? "hitterPaths" : "catcherPaths");
 
-       
+
         //oncomplete  string  for the name of a function to launch at the end of the animation
         //oncompletetarget    GameObject  for a reference to the GameObject that holds the "oncomplete" method
         //oncompleteparams    Object  for arguments to be sent to the "oncomplete" method
 
-        if(isHitted)
+        if (isHitted)
         {
             // 중간경로 계산 
-            hitterPaths[hitterPaths.Length - 2].position = (hitterPaths[hitterPaths.Length - 1].position + hitterPaths[hitterPaths.Length - 3].position) * 0.5f + Vector3.up * midPosHeight;
+            hitterPaths[hitterPaths.Length - 2].position = (hitterPaths[hitterPaths.Length - 1].position + hitterPaths[hitterPaths.Length - 3].position) * 0.7f + Vector3.up * midPosHeight;
 
+            Debug.Log(hitterPaths[1].transform.position);
+
+            iTween.MoveTo(gameObject, iTween.Hash("position", hitterPaths[0],
+                                                  "easeType", iTween.EaseType.easeOutSine,
+                                                  "time", 0.5f
+                                                 ));
+            
             iTween.MoveTo(gameObject, iTween.Hash("path", hitterPaths,
-                                              "easeType", iTween.EaseType.easeOutQuad,
-                                              "time", 5.5f,
-                                              "oncomplete", "BallDropped"));
+                                                  "easeType", iTween.EaseType.linear,
+                                                  "delay", 0.5f,
+                                                  "time", 5.0f,
+                                                  "oncomplete", "BallDropped",
+                                                  "movetopath", false));
         }
         else
         {
